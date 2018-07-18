e231cd78f6c85d11331e023195141c3ecab6b512
Kumar Agarwal <superkumario64@gmail.com>
2018-07-18 10:14:07 -0700

    create tab navigator between list decks and new decks

--- App.js
+++ App.js
@@ -2,13 +2,50 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import ListDecks from './components/ListDecks'
 import NewDeck from './components/NewDeck'
+import { createBottomTabNavigator } from 'react-navigation'
+
+const Tabs = createBottomTabNavigator(
+    {
+        ListDecks: {
+            screen: ListDecks,
+            navigationOptions: {
+                tabBarLabel: "DECKS"
+            }
+        },
+        NewDeck: {
+            screen: NewDeck,
+            navigationOptions: {
+                tabBarLabel: "NEW DECK"
+            }
+        }
+    },
+    {
+        navigationOptions: {
+            header: null
+        },
+        tabBarOptions: {
+            activeTintColor: "black",
+            style: {
+                height: 56,
+                backgroundColor: "white",
+                shadowColor: 'rgba(0, 0, 0, 0.24)',
+                shadowOffset: {
+                    width: 0,
+                    height: 3
+                },
+                shadowRadius: 6,
+                shadowOpacity: 1
+            }
+        }
+    }
+
+
+);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <NewDeck />
-      </View>
+        <Tabs />
     )
   }
 }
